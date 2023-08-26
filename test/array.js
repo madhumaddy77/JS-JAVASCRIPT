a9f55119cf110d0b9254d9cdae6fb8de1737666f
Madhu Bharadwaj <madhuhrr7348@gmail.com>
2023-08-27 01:18:01 +0530

    array folder

---
+++ test/array.js
@@ -0,0 +1,45 @@
+array1= new Array(1,2,3,4,5)
+// console.log(array1);
+
+array2=[2,4,6,8,10]
+// console.log(array2);
+
+// array2.push(1)
+// console.log(array2);
+
+// array2.pop(1)
+// console.log(array2);
+
+// array2.pop(6)
+// console.log(array2);
+// array2.shift(1)
+// console.log(array2);
+// array2.unshift(1)
+// console.log(array2);
+// array2.unshift(334567)
+// console.log(array2);
+// array2.shift(2)
+// console.log(array2);
+// array2.unshift()
+// console.log(array2);
+
+// console.log(array2.includes(2));
+// console.log(array2);
+
+// console.log(array2.indexOf(6));
+newarr2=array2.join()
+// console.log(array2);
+// console.log(typeof(newarr2));
+
+myarr3=[1,2,3,4,5,6,7,8,9,10]
+console.log(myarr3);
+
+slice1=myarr3.slice(1,4)
+console.log("1",slice1);
+
+console.log(myarr3);
+splice1=myarr3.splice(1,4)
+console.log("2",splice1);
+
+
+
