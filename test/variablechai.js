71c77ad5e01498bfeac88995e9d6c0a390cfb7d4
Madhu Bharadwaj <madhuhrr7348@gmail.com>
2023-08-26 23:53:43 +0530

    folder changed

---
+++ test/variablechai.js
@@ -0,0 +1,62 @@
+const firstName= "madhu"
+let secondName="bharadwaj"
+var lastName="sk"
+// table.console(firstName, secondName,lastName)
+// console.log(lastName)
+// console.log(firstName,secondName)
+let Email;
+// console.table([firstName,secondName,lastName,Email])
+
+gmit="33"
+let college=Boolean(gmit)
+// console.log(typeof college)
+
+num1=num2=num3=45
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+// console.log(2===3);
+
+const id=Symbol(123)
+const id1=Symbol(123)
+
+// console.log(id===id1);
+
+Name= "madhu"
+age= "20"
+course="Btech"
+
+// console.log(Name+age+course)
+
+// console.log(`my name is ${Name} and my age is ${age}, my course ${course}`);
+
+let flower= new String('Rose')
+
+// console.log(flower);
+
+// console.log(flower.toLowerCase());
+
+const url='htttps://madhu.gmit%20com'
+// console.log(url.replace('%20','.'));
+
+const str = 'madhu-bharadwaj-gmit-ai&ml-pavan-varahad-vamshi-sudeep-sameer-rishi-deepak-akash';
+
+editstr=str.split('m',4)
+// console.log(editstr);
+
+
+let  balance=Number(100)
+// console.log(balance.toFixed(3));
+// console.log(balance.toPrecision(2));
+// console.log(balance.toLocaleString());
+
+const min=35
+const max=30
+
+// console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+const Time= new Date();
+console.log(Time.toLocaleTimeString());
+console.log(Time.toTimeString());
+console.log(Time.toDateString())
