44343b3d68670cd3df29010b2c3ed15bfa67f64f
Madhu Bharadwaj <madhuhrr7348@gmail.com>
2023-08-27 13:15:40 +0530

    array 2nd class

--- test/array.js
+++ test/array.js
@@ -32,16 +32,35 @@ newarr2=array2.join()
 // console.log(typeof(newarr2));
 
 myarr3=[1,2,3,4,5,6,7,8,9,10]
-console.log(myarr3);
+// console.log(myarr3);
 
 slice1=myarr3.slice(1,4)
-console.log("1",slice1);
+// console.log("1",slice1);
 
-console.log(myarr3);
+// console.log(myarr3);
 splice1=myarr3.splice(1,4)
-console.log("2",splice1);
+// console.log("2",splice1);
 
-console.log("2",splice1);
+// console.log("2",splice1);
 
 
+flower=["rose","jasmin","sunflower","hibiscus"]
 
+fruits=["apple","bannana","watermelon","jackfruit"]
+
+console.log(flower,fruits);
+
+console.log(flower.concat(fruits))
+
+split_array=[...flower,...fruits] // split (important)
+console.log(split_array);
+
+
+mix_Array=[1,[2,3],4,[6,8],5,[8,10]]
+
+single_Array=mix_Array.flat(Infinity)
+console.log(single_Array);
+
+console.log(Array.isArray("MADHU"));
+console.log(Array.from("MADHU"));
+console.log(Array.of("MADHU"));
