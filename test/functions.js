b79d5a7ed04db7e68ba5a5573ac7639a335fb5f7
Madhu Bharadwaj <madhuhrr7348@gmail.com>
2023-10-14 00:41:22 +0530

    functions 2

--- test/functions.js
+++ test/functions.js
@@ -5,16 +5,16 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 // print()
 
-function add(num1, num2) {
-  return num1 + num2;
-//   return sum;
-}
+// function add(num1, num2) {
+//   return num1 + num2;
+// //   return sum;
+// }
 
- let num1 =1 //prompt("enter the number 1:");
- let num2 = 4//prompt("enter the number 2:");
-let result = add(num1, num2);
+//  let num1 =1 //prompt("enter the number 1:");
+//  let num2 = 4//prompt("enter the number 2:");
+// let result = add(num1, num2);
 
-console.log(result);
+// console.log(result);
 
 
 // function additionFunction(a, b) { 
@@ -25,3 +25,25 @@ console.log(result);
 // let num2 = 10; 
 // let sum = additionFunction(num1, num2); 
 // console.log("Sum of given numbers is :", sum);
+
+
+// function array(...array1){
+
+//   return array1
+
+// }
+// array1=8,2,3
+// console.log(array(2,3,4,5));
+
+ const user={
+ username : "madhu",
+  batch:1
+ }
+
+ function result(anything){
+  console.log(`${anything.username} is in the batch ${anything.batch}` );
+ }
+
+
+ result(user)
+
